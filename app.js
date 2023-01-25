e1b5158596b1fe8ce4cf0405d32f040a7a99905e
Duglianny Olavarria <dugliannydesiree1997@gmail.com>
2023-01-25 01:27:11 -0400

    main

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+    const enter = document.querySelector('#enter');
+    const completo = document.querySelector('#completo');
+
+    enter.addEventListener('click' , (e) => {
+      completo.classList.toggle('collapse')
+      
+      console.log(enter);
+      console.log(completo)
+    })
+
+    
